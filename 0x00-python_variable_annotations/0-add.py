636e0d7023dc9cc3c47822b30ca9ce34cb563f54
Mabongakev <kelvin.mabonga92@gmail.com>
2025-04-24 23:23:31 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""creating a function to add two numbers together"""
+
+def add_function(a: float, b: float) -> float:
+    """
+    Add two numbers together.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
